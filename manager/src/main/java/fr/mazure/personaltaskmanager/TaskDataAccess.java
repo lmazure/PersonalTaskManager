266d0cd6cc9694de3f61e096047f57c5fb194e7a
Laurent <laurent@gilgamesh>
2023-03-23 22:21:42 +0100

    Added  retrieval of task list

--- manager/src/main/java/fr/mazure/personaltaskmanager/TaskDataAccess.java
+++ manager/src/main/java/fr/mazure/personaltaskmanager/TaskDataAccess.java
@@ -1,9 +1,13 @@
 package fr.mazure.personaltaskmanager;
 
 import java.time.ZonedDateTime;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class TaskDataAccess {
     
@@ -16,19 +20,19 @@ public class TaskDataAccess {
     public void create(final TaskDatabaseDto data) throws ExistingRecordException {
         final TaskDatabaseRecord record = this.records.get(data.uuid());
         if (record != null) throw new ExistingRecordException("Record already exists (UUID=" + data.uuid() + ")");
-        this.records.put(data.uuid(), convertDataToRecord(data));
+        this.records.put(data.uuid(), convertDtoToRecord(data));
     }
 
     public void update(final TaskDatabaseDto data) throws UnexistingRecordException {
         final TaskDatabaseRecord record = this.records.get(data.uuid());
         if (record == null) throw new UnexistingRecordException("Unkwown record (UUID=" + data.uuid() + ")");
-        this.records.put(data.uuid(), convertDataToRecord(data));
+        this.records.put(data.uuid(), convertDtoToRecord(data));
     }
 
     public TaskDatabaseDto read(final UUID uuid) throws UnexistingRecordException {
         final TaskDatabaseRecord record = this.records.get(uuid);
         if (record == null) throw new UnexistingRecordException("Unkwown record (UUID=" + uuid + ")");
-        return convertRecordToDate(record);
+        return convertRecordToDto(record);
     }
 
     public void delete(final UUID uuid) throws UnexistingRecordException {
@@ -37,11 +41,22 @@ public class TaskDataAccess {
         this.records.remove(uuid);
     }
 
-    private TaskDatabaseRecord convertDataToRecord(final TaskDatabaseDto data) {
+    public List<TaskDatabaseDto> getTasks(final Predicate<TaskDatabaseDto> filter,
+                                          final Comparator<TaskDatabaseDto> sorter) {
+        return this.records
+                   .values()
+                   .stream()
+                   .map(TaskDataAccess::convertRecordToDto)
+                   .filter(filter)
+                   .sorted(sorter)
+                   .collect(Collectors.toList());
+    }
+
+    private static TaskDatabaseRecord convertDtoToRecord(final TaskDatabaseDto data) {
         return new TaskDatabaseRecord(data.uuid(), data.clientTimeStamp(), ZonedDateTime.now(), data.humanId(), data.humanDescription());
     }
 
-    private TaskDatabaseDto convertRecordToDate(final TaskDatabaseRecord record) {
+    private static TaskDatabaseDto convertRecordToDto(final TaskDatabaseRecord record) {
         return new TaskDatabaseDto(record.uuid(), record.clientTimeStamp(), record.humanId(), record.humanDescription());
     }
 }
--- manager/src/test/java/fr/mazure/personaltaskmanager/DataAccessTest.java
+++ manager/src/test/java/fr/mazure/personaltaskmanager/DataAccessTest.java
@@ -2,6 +2,7 @@ package fr.mazure.personaltaskmanager;
 
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Assertions;
@@ -65,7 +66,7 @@ public class DataAccessTest {
 
         access.create(dataIn1);
         access.delete(uuid);
-        UnexistingRecordException thrown = Assertions.assertThrows(UnexistingRecordException.class, () -> {
+        final UnexistingRecordException thrown = Assertions.assertThrows(UnexistingRecordException.class, () -> {
             access.read(uuid);
         });
 
@@ -85,7 +86,7 @@ public class DataAccessTest {
 
         access.create(dataIn1);
         access.delete(uuid);
-        UnexistingRecordException thrown = Assertions.assertThrows(UnexistingRecordException.class, () -> {
+        final UnexistingRecordException thrown = Assertions.assertThrows(UnexistingRecordException.class, () -> {
             access.delete(uuid);
         });
 
@@ -99,8 +100,8 @@ public class DataAccessTest {
 
         final UUID uuid = UUID.randomUUID();
         final ZonedDateTime timestamp = ZonedDateTime.now();
-        final String id = "ID";
-        final String description = "description";
+        final String id = "ID1";
+        final String description = "description1";
         final TaskDatabaseDto data = new TaskDatabaseDto(uuid, timestamp, id, description);
 
         final ZonedDateTime timestamp2 = ZonedDateTime.now();
@@ -108,12 +109,53 @@ public class DataAccessTest {
         final String description2 = "description2";
         final TaskDatabaseDto data2 = new TaskDatabaseDto(uuid, timestamp2, id2, description2);
 
-        ExistingRecordException thrown = Assertions.assertThrows(ExistingRecordException.class, () -> {
+        final ExistingRecordException thrown = Assertions.assertThrows(ExistingRecordException.class, () -> {
             access.create(data);
             access.create(data2);
         });
 
         Assertions.assertTrue(thrown.getMessage().contains("Record already exists"));
         Assertions.assertTrue(thrown.getMessage().contains(uuid.toString()));
+    }
+
+    @Test
+    void testGetList() throws ExistingRecordException {
+        final TaskDataAccess access = new TaskDataAccess();
+
+        final UUID uuid1 = UUID.randomUUID();
+        final ZonedDateTime timestamp = ZonedDateTime.now();
+        final String id = "ID1";
+        final String description = "descriptionB";
+        final TaskDatabaseDto dataIn1 = new TaskDatabaseDto(uuid1, timestamp, id, description);
+
+        final UUID uuid2 = UUID.randomUUID();
+        final ZonedDateTime timestamp2 = ZonedDateTime.now();
+        final String id2 = "ID2";
+        final String description2 = "descriptionC";
+        final TaskDatabaseDto dataIn2 = new TaskDatabaseDto(uuid2, timestamp2, id2, description2);
+
+        final UUID uuid3 = UUID.randomUUID();
+        final ZonedDateTime timestamp3 = ZonedDateTime.now();
+        final String id3 = "OD3";
+        final String description3 = "descriptionD";
+        final TaskDatabaseDto dataIn3 = new TaskDatabaseDto(uuid3, timestamp3, id3, description3);
+
+        final UUID uuid4 = UUID.randomUUID();
+        final ZonedDateTime timestamp4 = ZonedDateTime.now();
+        final String id4 = "ID2";
+        final String description4 = "descriptionA";
+        final TaskDatabaseDto dataIn4 = new TaskDatabaseDto(uuid4, timestamp4, id4, description4);
+        
+        access.create(dataIn1);
+        access.create(dataIn2);
+        access.create(dataIn3);
+        access.create(dataIn4);
+
+        final List<TaskDatabaseDto> dataOut = access.getTasks(t -> t.humanId().startsWith("ID"), (t1, t2) -> t1.humanDescription().compareTo(t2.humanDescription()));
+
+        Assertions.assertEquals(3, dataOut.size());
+        Assertions.assertEquals("descriptionA", dataOut.get(0).humanDescription());
+        Assertions.assertEquals("descriptionB", dataOut.get(1).humanDescription());
+        Assertions.assertEquals("descriptionC", dataOut.get(2).humanDescription());
     }
 }
